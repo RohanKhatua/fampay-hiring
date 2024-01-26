244b4dbb3d272fe2309615b4f0f24f69d7d86074
RohanKhatua <contact@rohankhatua.dev>
2024-01-26 21:16:47 +0530

    feat: paginated response for video query

---
+++ routes/getVideos.go
@@ -0,0 +1,32 @@
+package routes
+
+import (
+	"strconv"
+	"yt_api/database"
+	"yt_api/models"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetVideos(c *fiber.Ctx) error {
+	page, _ := strconv.Atoi(c.Query("page", "1"))
+	pageSize, _ := strconv.Atoi(c.Query("pageSize", "10"))
+
+	var videos []models.Video
+	result := database.Database.Db.Order("publish_time desc").Offset((page - 1) * pageSize).Limit(pageSize).Find(&videos)
+
+	if len(videos) == 0 {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": "No videos found",
+		})
+	}
+
+	if result.Error != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Error getting videos",
+		})
+	}
+
+	return c.JSON(videos)
+
+}
