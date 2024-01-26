4e33073233333e4bb6455d57e4fb7c88ad04dc6d
RohanKhatua <contact@rohankhatua.dev>
2024-01-26 21:14:06 +0530

    fix: PrepareStmt error

--- database/database.go
+++ database/database.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"yt_api/models"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -13,7 +14,7 @@ type DbInstance struct {
 	Db *gorm.DB
 }
 
-func GlobalActivationScope (db *gorm.DB) *gorm.DB {
+func GlobalActivationScope(db *gorm.DB) *gorm.DB {
 	return db.Where("is_activated = ?", true)
 }
 
@@ -29,10 +30,18 @@ func ConnectToDB() {
 
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbName)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		PrepareStmt: false,
+	})
 
 	if err != nil {
 		log.Fatal("Failed to connect to database")
+	}
+
+	migrateFlag := os.Getenv("MIGRATE")
+
+	if migrateFlag == "true" {
+		db.AutoMigrate(&models.Video{})
 	}
 
 	Database = DbInstance{Db: db}
